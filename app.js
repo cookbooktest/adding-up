8469ddd4c79e2f8cde25f75548a1a86cddb058ad
ckb <okage_samadesu@yahoo.co.jp>
2020-01-07 21:27:49 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ rl.on('line', (lineString) => {
     const year = parseInt(columns[0]);
     const prefecture = columns[1];
     const popu = parseInt(columns[3]);
-
     if (year === 2010 || year === 2015) {
       let value = prefectureDataMap.get(prefecture);
       if (!value) {
@@ -28,16 +27,16 @@ rl.on('line', (lineString) => {
       }
       prefectureDataMap.set(prefecture, value);
     }
-  });
-  rl.on('close', () => {
+});
+rl.on('close', () => {
     for (let [key, value] of prefectureDataMap) { 
-        value.change = value.popu15 / value.popu10;
-      }
-      const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
-      });
-      const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
-      });
-      console.log(rankingStrings);
-  });
+      value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+      return pair2[1].change - pair1[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value]) => {
+      return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    });
+    console.log(rankingStrings);
+});
